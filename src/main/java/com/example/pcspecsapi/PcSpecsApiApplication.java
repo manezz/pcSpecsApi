58b5285e20b42144a9c573bb55dd2d87fb5d5f26
Magnus Vangsgaard Norling <68356591+manezz@users.noreply.github.com>
2024-02-21 13:59:50 +0100

    first commit

---
+++ src/main/java/com/example/pcspecsapi/PcSpecsApiApplication.java
@@ -0,0 +1,13 @@
+package com.example.pcspecsapi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PcSpecsApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PcSpecsApiApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/pcspecsapi/PcSpecsApiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.pcspecsapi;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PcSpecsApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
